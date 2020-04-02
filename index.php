d4cebaec89e5eed797931090418ad4dd173030ef
kevin Leonard <kevinleonard.info@gmail.com>
2020-04-02 18:02:18 +0200

    index, header and footer

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <?php require_once('inc/head.html'); ?>
 
-<body>
-
+<body id="index">
 	<main>
-
+		<div class="overlay"></div>
+		<p class="logo">Test</p>
+		<h1>Donnez une nouvelle dimension à l'inclusion !</h1>
+		<a class="btn btn-white mb-4 mb-md-0" href="entreprise">Je suis une entreprise</a>
+		<a class="btn btn-white mb-4 mb-md-0" href="#">Je suis un candidat</a>
 	</main>
-
 </body>
 
 </html>
