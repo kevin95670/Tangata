81535df5614df4afae511ec5eebdb489b00e88ce
kevin Leonard <kevinleonard.info@gmail.com>
2020-04-08 16:17:25 +0200

    tarifs et avis page

---
+++ about.php
@@ -0,0 +1,45 @@
+<?php require_once('inc/head.html'); ?>
+
+<body id="about">
+
+	<?php require_once('inc/header.html'); ?>
+
+	<main>
+		<div class="container">
+			<div class="row position-relative background-image mb-5">
+				<div class="overlay"></div>
+				<div class="col-12">
+					<h1>À propos de Tangata</h1>
+				</div>
+			</div>
+			<div class="row about mb-5">
+				<div class="col-xs-12 col-md-6">
+					<i class="fas fa-compress-arrows-alt blue-circle-border mb-2"></i>
+					<h2 class="font-weight-bold mb-4">Aidez-nous à lancer notre projet et à le concrétiser.</h2>
+					<p class="mb-4">Tangata accompagne les travailleurs handicapés dans leur recherche d'emploi. notre plateforme met en avant les actions des entreprises en faveur de l'emploi des travailleurs handicapés et vous permet d'avoir accès aux informations adaptées à vos besoins.</p>
+				</div>
+				<div class="col-xs-12 col-md-6">
+					<i class="far fa-futbol blue-circle-border mb-2"></i>
+					<h2 class="font-weight-bold mb-4">Une plateforme centrée sur les loisirs existe déjà depuis 2019</h2>
+					<p class="mb-4">Des activités de loisirs accessibles en Île-de-France (spectacles, sports, culture, bien-être, ateliers) à associer à des services complémentaires adaptés à vos besoins (accompagnateurs, souffleurs d'images, location de voitures adaptées...).<br>
+					Une communauté pour partager vos bonnes pratiques et récolter des notes et avis fiables surt chaque offre pour faire votre choix en toute confiance.</p>
+					<a href="https://tangata.net/" rel="noopener" target="_blank" class="btn btns btn-blue">Accéder à Tangata.net</a>
+				</div>
+			</div>
+			<div class="row about mb-5">
+				<div class="col-md-12">
+					<p class="mb-4">Un site de <span class="font-weight-bold">mise en relation</span>entre les personnes touchées par le handicap, les aidants et les acteurs du handicap <span class="font-weight-bold">répondant à leurs besoins.</span></p>
+					<p class="mb-4"><span class="font-weight-bold">Un relais d'accélération</span>qui permet aux acteurs du handicap (associations, entreprises et start-ups) de <span class="font-weight-bold">gagner en visibilité et croître plus rapidement</span>, avec en option une proposition d'accompagnement par le réseau France Act for Impact de BNP Paribas.</p>
+				</div>
+			</div>
+			<div class="row about-team about mb-5">
+				<div class="col-md-12">
+					<i class="fas fa-users blue-circle-border mb-2"></i>
+					<h2 class="font-weight-bold mb-4">Une équipe dynamique et engagée</h2>
+					<img src="images/logo.png" class="img-fluid" alt="Tangata">
+				</div>
+			</div>
+		</div>
+	</main>
+
+	<?php require_once('inc/footer.html'); ?>
---
+++ tarifs.php
@@ -0,0 +1,35 @@
+<?php require_once('inc/head.html'); ?>
+
+<body id="tarifs">
+
+	<?php require_once('inc/header.html'); ?>
+
+	<main>
+		<div class="container">
+			<div class="row position-relative background-image mb-5">
+				<div class="overlay"></div>
+				<div class="col-12">
+					<h1>Une offre qui s'adapte à vos besoins et à votre budget</h1>
+				</div>
+			</div>
+			<div class="row tarifs mx-3 mb-5">
+				<div class="col-md-12 tarif-top border-bottom">
+					<img src="images/logo.png" class="img-fluid mb-2 mx-auto" alt="Tangata">
+					<h2 class="mb-3">Une offre complète</h2>
+					<p class="font-weight-bold first mb-0">6000 €</p>
+					<p class="mb-4 last">+ 2000 € par an</p>
+					<a href="contact" class="btn see-more btns btn-fill-blue mb-5">En savoir plus</a>
+				</div>
+				<div class="col-md-12 tarif-bottom pt-1">
+					<ul class="mt-4">
+						<li>Une page entreprise travaillée avec une équipe de spécialistes pour mettre en visibilité votre politique inclusive.</li>
+						<li>Un job board qualifié</li>
+						<li>Des candidats en adéquation avec votre demande</li>
+						<li>Un accès à un réseau de partenaires à tarif préférentiel</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</main>
+
+	<?php require_once('inc/footer.html'); ?>
