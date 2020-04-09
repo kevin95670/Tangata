53ef0a168ad3a82cfa11a8a8a45647c129d2b83b
kevin Leonard <kevinleonard.info@gmail.com>
2020-04-09 17:53:50 +0200

    partenaires et premier onglet entreprise page

---
+++ entreprise.php
@@ -0,0 +1,153 @@
+<?php require_once('inc/head.html'); ?>
+
+<body id="entreprise">
+
+	<?php require_once('inc/header.html'); ?>
+
+	<main>
+		<div class="container">
+			<div class="row position-relative background-image mb-5">
+				<div class="wrapper-logo position-absolute">
+					<div class="logo-entreprise position-relative">
+						<img class="position-absolute" src="images/logo.png" alt="Entreprise">
+					</div>
+				</div>
+			</div>
+			<div class="row mb-3">
+				<div class="col-md-12">
+					<h1>BNP Paribas</h1>
+					<p class="text-underline lieu">Nanterre (92)</p>
+				</div>
+			</div>
+			<div class="row entreprise-section">
+				<div class="col-4 no-padding">
+					<p class="active">Présentation</p>
+				</div>
+				<div class="col-4 no-padding">
+					<p>Offres d'emploi</p>
+				</div>
+				<div class="col-4 no-padding">
+					<p>Évènements</p>
+				</div>
+			</div>
+			<div class="entreprise-presentation">
+				<div class="row mt-4 mb-5 text-left">
+					<div class="col-12">
+						<h2>À propos de BNP Paris Cardif</h2>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas dignissim, odio id commodo consectetur, ligula ipsum blandit lorem, at lacinia massa ligula in enim. In hac habitasse platea dictumst. In tempor turpis elit, sit amet lacinia magna sollicitudin eget. Suspendisse sit amet ipsum scelerisque, porta libero ut, dictum turpis. Praesent blandit pulvinar nibh et iaculis. In accumsan turpis ante, non egestas dolor aliquet eget. Nunc laoreet lorem id elementum semper. In porta elit ligula, id sollicitudin nisl molestie a.</p>
+					</div>
+				</div>
+				<div class="row numbers position-relative py-4 mb-5">
+					<div class="overlay"></div>
+					<div class="col-6 col-md-3 mb-3">
+						<p><strong>1973</strong></p>
+						<p>Création de l'entité Cardif</p>
+					</div>
+					<div class="col-6 col-md-3 mb-3">
+						<p><strong>9750</strong></p>
+						<p>Collaborateurs dans le monde</p>
+					</div>
+					<div class="col-6 col-md-3 mb-3">
+						<p><strong>32</strong></p>
+						<p>Âge moyen au siège</p>
+					</div>
+					<div class="col-6 col-md-3 mb-3">
+						<p><strong>54 / 46</strong></p>
+						<p>Parité Hommes - Femmes</p>
+					</div>
+				</div>
+			</div>
+			<div class="row mb-5 mx-1 py-3 text-center unique-contact">
+				<div class="col-12">
+					<p class="mb-3"><strong>Un contact unique et privilégié</strong></p>
+					<p>Mme TARIN Stéphanie</p>
+					<p class="font-italic">Responsable mission handicap</p>
+					<p class="font-italic">Stephanie.tarin@bnpparibas.com</p>
+				</div>
+			</div>
+			<div class="row parole mb-4">
+				<div class="col-12">
+					<h2 class="mb-4">Vous avez la parole</h2>
+					<p>« J’ai eu la chance d’avoir d’abord une superbe tutrice de stage puis d’être dans une équipe formidable et bienveillante ! J’ai tout de suite bien été accueillie et me suis facilement intégrée au sein de l'équipe « Grande distribution et télécommunications ».
+					Je bénéficie d’horaires aménagés (10H30-17H) et d’un bureau réglable en hauteur.
+					Je trouve que Cardif est un groupe qui met tout en œuvre pour accueillir les personnes en situation de handicap. Au niveau professionnel, travailler au sein de Cardif est très enrichissant car j’ai pu découvrir plusieurs métiers et le côté international à travers ses filiales. »
+					</p>
+				</div>
+			</div>
+			<div class="row staff mb-5">
+				<div class="col-6">
+					<img class="img-fluid" src="images/logo.png" alt="Entreprise">
+				</div>
+				<div class="col-6 text-left staff-name">
+					<p><strong>Myriam Darmella</strong></p>
+					<p>Chargée d'études marketing</p>
+					<p>BNP Paris Cardif</p>
+				</div>
+			</div>
+			<div class="row handifacts mb-5">
+				<div class="col-12">
+					<h2 class="mb-3">Handi'facts</h2>
+					<ul class="list-style-none text-left pl-1">
+						<li><i class="fas fa-wheelchair"></i>Rampe d'accès</li>
+						<li><i class="fas fa-hand-peace"></i>Postes aménagés</li>
+						<li><i class="fas fa-users"></i>15+ collaborateurs rattachés à la mission handicap</li>
+						<li><i class="fas fa-hand-peace"></i>De nombreux avantages avec nos partenaires</li>
+					</ul>
+				</div>
+			</div>
+			<div class="row galeries mb-5">
+				<div class="col-12 col-md-6">
+					<div class="row galerie">
+						<div class="col-12">
+							<figure>
+								<img class="img-fluid" src="images/logo.png" alt="Entreprise">
+								<figcaption>Lisa - Community Manager</figcaption>
+							</figure>
+						</div>
+						<div class="col-6">
+							<figure>
+								<img class="img-fluid" src="images/logo.png" alt="Entreprise">
+								<figcaption>Lisa - Community Manager</figcaption>
+							</figure>
+						</div>
+						<div class="col-6">
+							<figure>
+								<img class="img-fluid" src="images/logo.png" alt="Entreprise">
+								<figcaption>Lisa - Community Manager</figcaption>
+							</figure>
+						</div>
+					</div>
+				</div>
+				<div class="col-12 col-md-6">
+					<div class="row galerie">
+						<div class="col-12">
+							<figure>
+								<img class="img-fluid" src="images/logo.png" alt="Entreprise">
+								<figcaption>Lisa - Community Manager</figcaption>
+							</figure>
+						</div>
+						<div class="col-6">
+							<figure>
+								<img class="img-fluid" src="images/logo.png" alt="Entreprise">
+								<figcaption>Lisa - Community Manager</figcaption>
+							</figure>
+						</div>
+						<div class="col-6">
+							<figure>
+								<img class="img-fluid" src="images/logo.png" alt="Entreprise">
+								<figcaption>Lisa - Community Manager</figcaption>
+							</figure>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="entreprise-offers">
+
+			</div>
+			<div class="entreprise-events">
+
+			</div>
+		</div>
+	</main>
+
+	<?php require_once('inc/footer.html'); ?>
